443b1afee7f900db60eb6f5ddae1bb1c64f82b08
Luan Menezes <menezluan120@gmail.com>
2024-10-11 07:28:18 -0300

    att

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+function mobile(){
+
 
 const menuIcon = document.getElementById('menu-icon');
 const navLinks = document.querySelector('.nav-links');
@@ -13,4 +15,13 @@ menuIcon.addEventListener('click', () => {
     } else {
         navLinks.style.display = 'none'; 
     }
-});
+})
+};
+
+function enviarmensagem(){
+    const mensagem = document.getElementById('mensagem').value;
+    const numeroWhatsApp = '+5574999108336';
+    const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem)}`;
+
+    window.open(url, '_blank');
+}
